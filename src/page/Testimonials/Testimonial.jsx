b1203627adf0c018cad93f777d5f3ee02ab8fe23
Bajlar01~ <bajlarrahman24@gmail.com>
2023-07-02 02:37:20 +0600

    testimonials swiper added

--- src/page/Testimonials/Testimonial.jsx
+++ src/page/Testimonials/Testimonial.jsx
@@ -7,13 +7,8 @@ const Testimonial = ({ review }) => {
   const { name, image, details, rating } = review;
 
   return (
-    <div
-      data-aos="zoom-in"
-      data-aos-easing="ease-out-cubic"
-      data-aos-duration="2000"
-      className="card bg-base-100 shadow-md mt-4"
-    >
-      <div className="card-body items-center text-center">
+    <div className="card bg-base-100 shadow-md mt-4">
+      <div className="card-body bg-[#faf6f6] items-center text-center">
         <FaQuoteLeft className="text-2xl text-yellow-500" />
         <p>{details}</p>
         <div className="w-[30px] border-b-4 text-gray-400"></div>
--- src/page/Testimonials/Testimonials.jsx
+++ src/page/Testimonials/Testimonials.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Testimonial from "./Testimonial";
 import SectionTitle from "../../components/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
 
 const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
@@ -17,11 +24,34 @@ const Testimonials = () => {
     <>
       <section id="" className="py-12 px-4 bg-white md:px-28">
         <SectionTitle heading={"Testimonials"} />
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-10">
+        <div className="md:grid grid-cols-1 md:grid-cols-3 gap-4 mt-10 hidden">
           {reviews.map((review) => (
             <Testimonial review={review} key={review._id}></Testimonial>
           ))}
         </div>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <div className="block mt-10 md:hidden">
+              {reviews.slice(0, 1).map((review) => (
+                <Testimonial review={review} key={review._id}></Testimonial>
+              ))}
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="block mt-10 md:hidden">
+              {reviews.slice(1, 2).map((review) => (
+                <Testimonial review={review} key={review._id}></Testimonial>
+              ))}
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="block mt-10 md:hidden">
+              {reviews.slice(2, 3).map((review) => (
+                <Testimonial review={review} key={review._id}></Testimonial>
+              ))}
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </section>
       <p id="contact" className="text-transparent mb-[53px]">
         hidden
